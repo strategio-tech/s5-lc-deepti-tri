10bcaa10421b86faa8725f2a0c4a1ddd0318cb74
Deeps <100620410+deepti-tri@users.noreply.github.com>
2022-11-04 13:39:32 -0500

    added the code

--- src/main/java/tech/strategio/LoanCalculator.java
+++ src/main/java/tech/strategio/LoanCalculator.java
@@ -12,8 +12,12 @@ public class LoanCalculator {
      * @return ???
      */
     static int getRemainingAmountIn3Months(int amount) {
-        // TODO: Rewrite this method
-        return -1;
+        int remainingAmount = 0;
+        for (int i = 0; i < 3; i++) {
+            remainingAmount = amount - amount / 10;
+            amount = remainingAmount;
+        }
+        return remainingAmount;
     }
 
     public static void main(String[] args) {
